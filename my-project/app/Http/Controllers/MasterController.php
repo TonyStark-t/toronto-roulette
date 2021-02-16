59b80612b6a56990e31d776c9be93811d0b8617d
USER-R8UQN8ECGR\user <bumband00ap@gmail.com>
2021-02-16 21:46:50 +0900

    コントローラーの追加およびapiのルーティング追加

--- my-project/app/Http/Controllers/MasterController.php
+++ my-project/app/Http/Controllers/MasterController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\MasterService;
 
 class MasterController extends Controller
 {
-    //
+    private MasterService $service;
+
+    public function __construct(MasterService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getList()
+    {
+        $this->service->getList();
+    }
 }
